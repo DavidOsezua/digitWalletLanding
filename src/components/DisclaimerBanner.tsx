32c0869635c63e4dd46272e21428be68aff507b2
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2025-12-16 08:37:44 +0100

    ...

--- src/components/DisclaimerBanner.tsx
+++ src/components/DisclaimerBanner.tsx
@@ -1,25 +1,25 @@
 const DisclaimerBanner = () => {
   return (
     <section className="sticky top-20 md:top-25 z-40 px-4 lg:px-0">
-    <div
-      className="max-w-7xl px-3  py-2 md:px-6 md:py-4 backdrop-blur-2xl mx-auto rounded-3xl"
-      style={{
-        backgroundColor: "rgba(255, 255, 255, 0.05)",
-      }}
-    > 
-      <p className="text-white text-sm md:text-base">
-        Don't invest unless you're prepared to lose all the money you invest.
-        This is a high-risk investment and you should not expect to be protected
-        if something goes wrong.{" "}
-        <a
-          href="#learn-more"
-          className="underline hover:opacity-80 transition-opacity"
-          style={{ color: "var(--color-primary-300)" }}
-        >
-          Take 2 minutes to learn more
-        </a>
-      </p>
-    </div>
+      <div
+        className="max-w-7xl px-3  py-2 md:px-6 md:py-4 backdrop-blur-2xl mx-auto rounded-3xl"
+        style={{
+          backgroundColor: "rgba(255, 255, 255, 0.05)",
+        }}
+      >
+        <p className="text-white text-[12px] md:text-base">
+          Don't invest unless you're prepared to lose all the money you invest.
+          This is a high-risk investment and you should not expect to be
+          protected if something goes wrong.{" "}
+          <a
+            href="#learn-more"
+            className="underline hover:opacity-80 transition-opacity"
+            style={{ color: "var(--color-primary-300)" }}
+          >
+            Take 2 minutes to learn more
+          </a>
+        </p>
+      </div>
     </section>
   );
 };
