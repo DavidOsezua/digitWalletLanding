ed9d37ce4bfb626b29428b152ea4bc7ebaecbe33
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2025-12-17 08:16:50 +0100

    created homepage and and loginLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,15 @@
-import Navbar from "./components/Navbar";
-import DisclaimerBanner from "./components/DisclaimerBanner";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import TailoredSection from "./components/TailoredSection";
-import FAQSection from "./components/FAQSection";
-import DigitalFinance from "./components/DigitalFinance";
-import DisclaimerComponent from "./components/DisclaimerComponent";
-import MainFooter from "./components/MainFooter";
-import Footer from "./components/Footer";
-import Warning from "./components/Warning";
-import OnboardingSteps from "./components/OnboardingSteps";
-import ComplianceSection from "./components/ComplianceSection";
-import LogoTicker from "./components/LogoTicker";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import LoginLayout from "./pages/LoginLayout";
 
 const App = () => {
   return (
-    <div className=" relative">
-      {/* Top-left Flashlight Effect */}
-      <div
-        className="fixed -top-96 -left-96 w-200 h-200 rounded-full blur-3xl opacity-30 pointer-events-none z-0"
-        style={{ backgroundColor: "#7A72FF" }}
-      ></div>
-
-      <Navbar />
-      <DisclaimerBanner />
-      <Hero />
-      <Features />
-      <TailoredSection />
-      <ComplianceSection />
-      <LogoTicker />
-      <OnboardingSteps />
-      <FAQSection />
-      <DigitalFinance />
-      <DisclaimerComponent />
-      <MainFooter />
-      <Warning />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginLayout />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/LoginLayout.tsx
@@ -0,0 +1,12 @@
+const LoginLayout = () => {
+  return (
+    <div
+      className="min-h-screen"
+      style={{ backgroundColor: "#1a1d2e" }}
+    >
+      {/* Login content will go here */}
+    </div>
+  );
+};
+
+export default LoginLayout;
