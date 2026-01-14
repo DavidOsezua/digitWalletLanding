4d70a7bd51d6d79bfdae55166ac45c114f66b806
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2026-01-14 23:58:53 +0100

    ...

--- src/components/DisclaimerBanner.tsx
+++ src/components/DisclaimerBanner.tsx
@@ -4,7 +4,7 @@ const DisclaimerBanner = () => {
   return (
     <section className="sticky top-20 md:top-25 z-30 px-4 lg:px-0">
       <div className="max-w-7xl px-3 bg-gradient-card-2  py-2 md:px-6 md:py-4 backdrop-blur-2xl mx-auto rounded-3xl">
-        <p className="text-white text-[15px] font-bold md:text-[20px]  ">
+        <p className="text-white text-[15px] font-bold md:text-[18px]  ">
           Don't invest unless you're prepared to lose all the money you invest.
           This is a high-risk investment and you should not expect to be
           protected if something goes wrong.{" "}
